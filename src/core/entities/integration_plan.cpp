a92f522c482c19d8f1e78f0a6f7c278387e2130a
lorenzo cozza <lorenzo.cozza@hotmail.com>
2016-09-20 23:40:32 +0200

    [java->qt] - import something from datapackage

--- src/core/entities/integration_plan.cpp
+++ src/core/entities/integration_plan.cpp
@@ -7,3 +7,13 @@ IntegrationPlan::IntegrationPlan(QObject *parent):QObject(parent)
 {
 
 }
+
+IntegrationPlan::IntegrationPlan(QString summary, QDate due_date, QDate start_date, QString cw, QDate merge_date, QObject *parent)
+:QObject(parent)
+{
+    this->m_summary = summary;
+    this->m_due_date = due_date;
+    this->m_start_date = start_date;
+    this->m_cw = cw;
+    this->m_merge_date = merge_date;
+}
--- src/core/entities/integration_plan.h
+++ src/core/entities/integration_plan.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 #include <QString>
+#include <QDate>
+#include <QList>
+#include "build.h"
 
 class IntegrationPlan : public QObject{
 
@@ -10,12 +13,35 @@ class IntegrationPlan : public QObject{
 
 public:
     IntegrationPlan(QObject *parent);
+    IntegrationPlan(QString summary, QDate due_date, QDate start_date, QString cw, QDate merge_date, QObject *parent);
     static const QString RELEASE;
     static const QString DEV_DROP;
+    //todo
+    const QString toString();
+    const QString getDateDDMMYYYY(QDate date);
+    const QDate getRealStartDate(); /*giono della prima attività*/
+    const QDate getRealDueDate();   /*giorno dell'ultima attività*/
+    bool checkIntegrationPlan();
+    int compareDate(QDate d1, QDate d2); /*return -1 for d1 ; 1 for d2 ; 0 d1==d2*/
 
 public slots:
 
 private:
+    QString m_summary;
+    QDate m_due_date;
+    QDate m_start_date;
+    QString m_cw;
+    QDate m_merge_date;
+    QList<Build> m_builds;
+    bool m_noMerge;
+    QString m_security_level;
+    QString m_assignees;
+    QString m_pic;
+    QString sub_project;
+    QString m_label;
+    QString m_priority;
+    QString m_stp_type;
+    QString m_html_code;
 
 signals:
 
